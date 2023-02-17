bd4e112c5b3515e5292e07738fcf8c6a04592909
Eliza Thomas <elizamthomas1994@gmail.com>
2023-02-17 16:01:31 +0000

    Created a basic introductory menu

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class BasicCalculator {
+	public static void main(String args[]){
+		Scanner scan = new Scanner (System.in);
+
+		System.out.println("Hello and welcome to the Calculator app!");
+		System.out.println("Please select from one of the following options");
+		System.out.println("Press '1' for addition");
+		System.out.println("Press '2' for subtraction");
+		System.out.println("Press '3' for multiplication");
+		System.out.println("Press '4' for division");
+
+		int selection = scan.nextInt();
+	}
+	
+
 	public int add (int numberA, int numberB){
 		return numberA + numberB;
 	}

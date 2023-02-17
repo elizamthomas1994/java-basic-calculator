e8255049870e6588c9df3e3decc3d4774ccf05c6
Eliza Thomas <elizamthomas1994@gmail.com>
2023-02-17 16:14:46 +0000

    Seperated introduction and menu into two different methods

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -2,16 +2,27 @@ import java.util.Scanner;
 
 public class BasicCalculator {
 	public static void main(String args[]){
-		Scanner scan = new Scanner (System.in);
-
 		System.out.println("Hello and welcome to the Calculator app!");
-		System.out.println("Please select from one of the following options");
-		System.out.println("Press '1' for addition");
-		System.out.println("Press '2' for subtraction");
-		System.out.println("Press '3' for multiplication");
-		System.out.println("Press '4' for division");
+		menu();
+	}
 
-		int selection = scan.nextInt();
+	public static void menu() {
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(System.in);
+
+			System.out.println("Please select from one of the following options");
+			System.out.println("Press '1' for addition");
+			System.out.println("Press '2' for subtraction");
+			System.out.println("Press '3' for multiplication");
+			System.out.println("Press '4' for division");
+
+			int selection = scanner.nextInt();
+		}
+		finally {
+			if(scanner!=null)
+				scanner.close();
+		}
 	}
 	
 

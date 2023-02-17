d80410e6780b25d1ffcb6f4de53e9e10f5d58ffc
Admin <admin@admins-air.home>
2023-02-17 15:44:18 +0000

    Created very basic methods that add, subtract, multiply and divide two numbers

---
+++ BasicCalculator.java
@@ -0,0 +1,18 @@
+public class BasicCalculator {
+	public int add (int numberA, int numberB){
+		return numberA + numberB;
+	}
+
+	public int subtract (int numberA, int numberB){
+		return numberA - numberB;
+	}
+
+	public int multiply (int numberA, int numberB){
+		return numberA * numberB;
+	}
+
+	public int divide (int numberA, int numberB){
+		return numberA / numberB;
+	}
+
+}
